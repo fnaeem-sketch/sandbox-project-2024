7c2dc5d4b2cbf2ee1f0116e43a0b84e5d882233e
Furqaan Naeem <fnaeem2@gmail.com>
2024-06-10 19:09:14 +0100

    add public route table

---
+++ route-tables.tf
@@ -0,0 +1,13 @@
+//Public route table
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
